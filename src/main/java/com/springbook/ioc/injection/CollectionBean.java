e2f4555aa22ace80261192378c663671b8da6957
JmSeastar <Jseastar92@gmail.com>
2020-01-11 23:35:49 +0900

    class4 4.4 set Collection Object

--- src/main/java/com/springbook/ioc/injection/CollectionBean.java
+++ src/main/java/com/springbook/ioc/injection/CollectionBean.java
@@ -1,15 +1,25 @@
 package com.springbook.ioc.injection;
 
+import java.util.List;
 import java.util.Properties;
 
 public class CollectionBean {
-	private Properties addressList;
+//	private Properties addressList;
+	public List<String> addressList;
 
-	public void setAddressList(Properties addressList) {
+	public void setAddressList(List<String> addressList) {
 		this.addressList = addressList;
 	}
 
-	public Properties getAddressList() {
+	public List<String> getAddressList(){
 		return addressList;
 	}
+
+//	public void setAddressList(Properties addressList) {
+//		this.addressList = addressList;
+//	}
+
+//	public Properties getAddressList() {
+//		return addressList;
+//	}
 }
