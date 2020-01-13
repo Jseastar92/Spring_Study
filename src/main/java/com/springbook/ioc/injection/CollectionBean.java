92a9b02eb028f65197d9953262d6e065b8cdcac4
JmSeastar <Jseastar92@gmail.com>
2020-01-13 23:19:08 +0900

    class4 끝. List, Set, Map, Properties 타입 매핑

--- src/main/java/com/springbook/ioc/injection/CollectionBean.java
+++ src/main/java/com/springbook/ioc/injection/CollectionBean.java
@@ -1,25 +1,45 @@
 package com.springbook.ioc.injection;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
+import java.util.Set;
 
 public class CollectionBean {
-//	private Properties addressList;
-	public List<String> addressList;
 
-	public void setAddressList(List<String> addressList) {
+	/*===== 4.4.4 Properties Type Mapping =====*/
+	private Properties addressList;
+
+	public void setAddressList(Properties addressList) {
 		this.addressList = addressList;
 	}
 
-	public List<String> getAddressList(){
+	public Properties getAddressList() {
 		return addressList;
 	}
+
+	/*===== 4.4.3 Map Type Mapping =====*/
+	/*private Map<String, String> addressList;
+
+	public void setAddressList(Map<String, String> addressList ) {
+		this.addressList = addressList;
+	}
+	public Map<String, String> getAddressList() {
+		return addressList;
+	}
+	*/
+
+
+	/*===== 4.4.2 Set Type Mapping =====
+
+	private Set< String> addressList;
+	public void setAddressList(Set<String> addressList) {
+		this.addressList = addressList;
+	}
+	=====          				 =====*/
 
 //	public void setAddressList(Properties addressList) {
 //		this.addressList = addressList;
 //	}
 
-//	public Properties getAddressList() {
-//		return addressList;
-//	}
 }
--- src/main/java/com/springbook/ioc/injection/CollectionBeanClient.java
+++ src/main/java/com/springbook/ioc/injection/CollectionBeanClient.java
@@ -1,5 +1,6 @@
 package com.springbook.ioc.injection;
 
+import java.util.List;
 import java.util.Properties;
 
 import org.springframework.context.support.AbstractApplicationContext;
@@ -9,13 +10,14 @@ public class CollectionBeanClient {
 	public static void main(String[] args) {
 		AbstractApplicationContext factory = new GenericXmlApplicationContext("applicationContext.xml");
 		CollectionBean bean = (CollectionBean) factory.getBean("collectionBean");
-		
+
+//		List<String> addressList = bean.getAddressList();
 		Properties addressList = bean.getAddressList();
-		
-		for( String key : addressList.stringPropertyNames() ){
-			System.out.println(String.format("Å° : %s, °ª : %s", key, addressList.get(key)) );
-		}
-		
+		System.out.println(addressList);
+//		for (String address : addressList) {
+//			System.out.println(address.toString());
+//		}
+
 		factory.close();
 	}
 }
