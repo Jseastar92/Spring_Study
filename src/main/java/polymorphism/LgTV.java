235b6481ad73425ff1057b8a31bffd498595f9e9
jSeastar <Jseastar92@gmail.com>
2020-01-14 21:24:38 +0900

    Class 5 : Annotation DI

--- src/main/java/polymorphism/LgTV.java
+++ src/main/java/polymorphism/LgTV.java
@@ -1,6 +1,26 @@
 package polymorphism;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+
+@Component("tv")
 public class LgTV implements TV {
+
+//    @Resource(name = "apple")
+//    private Speaker speaker;
+
+
+    /*@Qualifier("apple")*/
+    @Autowired
+    private Speaker speaker;
+
+
+    public LgTV() {
+        System.out.println("===> LgTv 객체 생성");
+    }
 
     public void powerOn() {
         System.out.println("Lg TV -- power On");
@@ -10,14 +30,20 @@ public class LgTV implements TV {
         System.out.println("Lg TV -- power Off");
 
     }
+//
+//    public void volumeUp() {
+//        System.out.println("Lg TV -- volume up");
+//
+//    }
+//
+//    public void volumeDown() {
+//        System.out.println("Lg TV -- volume down");
+//    }
 
     public void volumeUp() {
-        System.out.println("Lg TV -- volume up");
-
+        speaker.volumeUp();
     }
-
     public void volumeDown() {
-        System.out.println("Lg TV -- volume down");
-
+        speaker.volumeDown();
     }
 }
