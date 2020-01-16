23f4e0528b3905a76696fb8f54c2c4184fdbf3c0
jSeastar <Jseastar92@gmail.com>
2020-01-16 23:54:17 +0900

    Class 6 비즈니스 컴포넌트 실습

---
+++ src/main/java/com/springbook/biz/board/BoardVO.java
@@ -0,0 +1,14 @@
+package com.springbook.biz.board;
+
+import java.util.Date;
+
+//View Object
+public class BoardVO {
+
+    private int seq;
+    private String title;
+    private String writer;
+    private String content;
+    private Date regDate;
+    private int cnt;
+}
